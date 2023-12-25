62a3747b947f92f47b1a61339479583076762cfc
David <79108364+Phpvarious@users.noreply.github.com>
2023-12-25 21:03:09 +0100

    test

--- furaxworld/css-js/js/custom.js
+++ furaxworld/css-js/js/custom.js
@@ -16,15 +16,12 @@ $(document).ready(function() {
   $("a.top-scroll").on('click', function(event) {
     console.log(this)
     console.log(this.hash)
-    if (this.hash !== "") {
+    //if (this.hash !== "") {
       event.preventDefault();
-      var hash = this.hash;
       $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 2000, function(){
-        window.location.hash = hash;
-      });
-    }
+        scrollTop: 0
+      }, 2000);
+    //}
   });
 });
 }(jQuery));
